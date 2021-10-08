43a29ee6fee245b039fd0aab2d3ed4ebb072d5f8
Tharsikan98 <tharsikan.18@business.mrt.ac.lk>
2021-10-08 22:47:26 +0530

    My first python project in github

---
+++ basic.py
@@ -0,0 +1,12 @@
+
+def students_names():
+     a=input("Enter The Fist Name:")
+     b=input("Enter The Second Name:")
+     c=a+b
+     return c
+s=students_names()
+print("Full Name : ", s)
+
+
+
+
